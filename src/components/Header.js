af869a7f3934b5a9f52492c540f04f878626724e
Rifky Niyas <63934498+MohammadRifky@users.noreply.github.com>
2023-08-04 19:53:08 +0530

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,36 @@
+import Image from "next/image";
+const Header = () => {
+  return (
+    <header className="flex items-center max-w-6xl mx-auto">
+      <a href="/" className="relative h-4 w-16">
+        <Image src="/images/logo.svg" fill={true} />
+      </a>
+      <nav>
+        <ul className="flex items-center">
+          <li>
+            <a href=""></a>
+            Home
+          </li>
+          <li>
+            <a href="#">What is GPT?</a>
+          </li>
+          <li>
+            <a href="#">Open AI</a>
+          </li>
+          <li>
+            <a href="#">Case Studies</a>
+          </li>
+          <li>
+            <a href="#">Library</a>
+          </li>
+        </ul>
+      </nav>
+      <div>
+        <a href="#">Sign in</a>
+        <a href="#">Sign up</a>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
