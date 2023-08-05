0e029cb28da20ed746b980512ad8319c5ee749a7
Rifky Niyas <63934498+MohammadRifky@users.noreply.github.com>
2023-08-05 17:46:19 +0530

    completed blogs component

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -3,17 +3,50 @@ import React from "react";
 
 const MainBlog = () => {
   return (
-    <article className="row-span-2">
-      <figure>
-        <Image src="" />
+    <article className="row-span-2 bg-blue-bg flex flex-col">
+      <figure className="relative w-auto h-72">
+        <Image
+          src="/images/blog-image-1.jpg"
+          fill={true}
+          style={{ objectFit: "cover" }}
+        />
       </figure>
-      <p>Main Article</p>
+      <div className="px-6 flex flex-col grow">
+        <p className="mt-8 mb-3 text-xs font-bold">Sep 26, 2021</p>
+        <h3 className="text-2xl font-extrabold">
+          GPT-3 and Open AI is the future. Let us exlore how it is?
+        </h3>
+
+        <a href="#" className="block mt-auto mb-6 text-xs font-bold">
+          Read Full Article
+        </a>
+      </div>
     </article>
   );
 };
 
 const BlogItem = () => {
-  return <article>Single Blog Article</article>;
+  return (
+    <article className="bg-blue-bg">
+      <figure className="relative w-auto h-32">
+        <Image
+          src="/images/blog-image-1.jpg"
+          fill={true}
+          style={{ objectFit: "cover" }}
+        />
+      </figure>
+      <div className="px-6">
+        <p className="mt-4 mb-2 text-xs font-bold">Sep 26, 2021</p>
+        <h3 className="text-2xl font-extrabold">
+          GPT-3 and Open AI is the future. Let us exlore how it is?
+        </h3>
+
+        <a href="#" className="block mt-10 mb-6 text-xs font-bold">
+          Read Full Article
+        </a>
+      </div>
+    </article>
+  );
 };
 const Blogs = () => {
   return (
@@ -21,7 +54,7 @@ const Blogs = () => {
       <h2 className="text-6xl font-extrabold mb-32 text-transparent bg-main-gradient bg-clip-text">
         A lot is happening, <br /> We are blogging about it.
       </h2>
-      <div className="grid grid-cols-3 grid-rows-2">
+      <div className="grid grid-cols-3 grid-rows-2 gap-x-14 gap-y-12">
         <MainBlog />
         <BlogItem />
         <BlogItem />
